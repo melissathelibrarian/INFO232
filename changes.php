ea0268480d16ab86d39adff1ca3759a73775bbff
melissathelibrarian <65092693+melissathelibrarian@users.noreply.github.com>
2020-05-09 15:20:45 -0600

    Add files via upload

---
+++ changes.php
@@ -0,0 +1,66 @@
+<?php include('includes/process.php');?>
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Opportunities for Improvement</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="stylesheet" href="style.css">
+   
+</head>
+
+<body>
+
+<header class="banner">
+<?php include('includes/banner.php');?>
+
+</header>
+
+<div class="content-wrapper">
+
+
+
+<h2>Recommended Changes</h2>
+<main>
+<h3><em>Lexicon</em></h3>
+<p>As can be seen from reading the Strategic Initiatives, the LA County Library system is already on its way to emphasizing the educational role of libraries. For example, Strategic Initiative 2, Affirm the Library as a Center for Learning - Address our communities’ learning and literacy needs through classes, events and online technology, already uses words such as “learning”, “classes” and “events” to describe the offerings at the library. This initiative even goes so far as to call libraries centers for learning.
+
+Additionally, the Strategic Initiatives refer to library users as “customers”. Gross (2013) asserts that the use of the term “customer” is beneficial to libraries because it “furthers [libraries’] quest to be perceived as a solid financial investment” (p. 37).
+
+The LA County Library System’s Strategic Initiatives do an excellent job at utilizing a lexicon that makes their intended mission sound clear and valuable to the public, without using confusing library jargon. We would recommend only the following small tweaks to the language of Strategic Initiatives 1, 4 and 6:
+ </p> 
+ <img src="images/initiative4.jpg">
+
+<p>Many libraries use the word “database”, but Gross suggests moving away from this and instead using the phrase “specialized online research tools”, which breaks down what a database actually is and what function it will be providing.</p>
+<img src="images/databases.jpg">
+<p>On the “Library Locator” section of their webpage, LA County Library lists their three bookmobiles. Gross would suggest that the word “bookmobile” should be swapped out for the phrase “mobile branch”, which conveys to the community that this vehicle has all the same offerings as a brick and mortar branch.</p>
+<img src="images/bookmobile.jpg">
+</main>
+<aside class="sidebar">
+	
+<h4><em>Programming vs Workshop</em></h4>
+<p>Taken from their event calendar, this listing uses the word “workshop” instead of “program”, which is in keeping with Gross’ suggestions.</p>
+<img src="images/program1.jpg">
+<p>However, in this listing, the word “program” is used multiple times in the last line. This could easily be rewritten to read, “Parents: please be aware that refreshments will be served at this class. A list of ingredients will be available at the class.”</p>
+<img src="images/program2.jpg">
+
+
+
+</div>
+	
+	</aside>
+
+<h3>Conclusion</h3>
+
+<p>An examination of the LA County Library showed that they are well on their way to utilizing a lexicon that conveys the library’s valuable role as an education center in the community. The language used is clear, concise and steers away from typical library jargon that can cause users to feel lost and overwhelmed.</p>
+
+
+</div>
+
+<footer class="site-footer">
+<?php include('includes/copyright.php');?>
+
+</footer>
+
+</body>
+</html>
